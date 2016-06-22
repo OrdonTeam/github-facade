21979ecc53151a2da21aa5d5627067bae5d7f67e
Karol Kowalski <ordon1313@gmail.com>
2016-06-22 21:17:14 +0200

    GitHubRepositoryDetailsController

---
+++ src/test/java/github/repositories/details/controller/GitHubRepositoryDetailsControllerTest.java
@@ -0,0 +1,44 @@
+package github.repositories.details.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {GitHubRepositoryDetailsControllerTest.TestConfiguration.class})
+public class GitHubRepositoryDetailsControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void shouldRunEndpoint() throws Exception {
+        mockMvc.perform(get("/repositories"))
+                .andExpect(status().isOk());
+    }
+
+    @Configuration
+    @ComponentScan
+    public static class TestConfiguration {
+
+    }
+}
