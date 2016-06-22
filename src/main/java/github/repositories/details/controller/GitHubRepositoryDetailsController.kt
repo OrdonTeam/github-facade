ccf234fd1d861a7248f9d5b5fbc7770d8f5b388c
Karol Kowalski <ordon1313@gmail.com>
2016-06-23 00:57:25 +0200

    shouldReturnJson

--- src/main/java/github/repositories/details/controller/GitHubRepositoryDetailsController.kt
+++ src/main/java/github/repositories/details/controller/GitHubRepositoryDetailsController.kt
@@ -1,20 +1,18 @@
 package github.repositories.details.controller
 
+import github.repositories.details.GitHubRepositoryDetails
 import github.repositories.details.GitHubRepositoryDetailsService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
-@Controller
+@RestController
 class GitHubRepositoryDetailsController @Autowired constructor(val service: GitHubRepositoryDetailsService) {
 
     @RequestMapping(value = "/repositories/{owner}/{repositoryName}", method = arrayOf(RequestMethod.GET))
     fun repositoryDetails(
             @PathVariable("owner") owner: String,
-            @PathVariable("repositoryName") repositoryName: String): String {
-        return service.getRepositoryDetails(owner, repositoryName, Locale.US).toString()
+            @PathVariable("repositoryName") repositoryName: String): GitHubRepositoryDetails {
+        return service.getRepositoryDetails(owner, repositoryName, Locale.US)
     }
 }
--- src/test/java/github/repositories/details/controller/GitHubRepositoryDetailsControllerTest.kt
+++ src/test/java/github/repositories/details/controller/GitHubRepositoryDetailsControllerTest.kt
@@ -1,8 +1,7 @@
 package github.repositories.details.controller
 
-import com.nhaarman.mockito_kotlin.any
+import com.google.gson.Gson
 import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
 import github.repositories.details.GitHubRepositoryDetailsService
 import github.repositories.details.newGitHubRepositoryDetails
@@ -10,6 +9,7 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
@@ -17,11 +17,10 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
-
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import java.util.*
 
 @RunWith(SpringJUnit4ClassRunner::class)
@@ -41,14 +40,15 @@ class GitHubRepositoryDetailsControllerTest {
     }
 
     @Test
-    fun shouldRunEndpoint() {
-        whenever(service.getRepositoryDetails(any(), any(), any())).thenReturn(newGitHubRepositoryDetails())
-        mockMvc.perform(get("/repositories/OrdonTeam/ogame-api")).andExpect(status().isOk)
-        verify(service).getRepositoryDetails("OrdonTeam", "ogame-api", Locale.US)
+    fun shouldReturnJson() {
+        whenever(service.getRepositoryDetails("OrdonTeam", "ogame-api", Locale.US)).thenReturn(newGitHubRepositoryDetails())
+        mockMvc.perform(get("/repositories/OrdonTeam/ogame-api"))
+                .andExpect(content().json(Gson().toJson(newGitHubRepositoryDetails())))
     }
 
-    @Configuration
+    @EnableAutoConfiguration
     @ComponentScan
+    @Configuration
     open class TestConfiguration {
 
         @Bean
