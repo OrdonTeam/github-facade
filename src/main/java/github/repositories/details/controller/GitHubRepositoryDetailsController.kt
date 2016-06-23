e797481437db4eab20cd9564f13ba4ad2c88ba85
Karol Kowalski <ordon1313@gmail.com>
2016-06-23 19:45:53 +0200

    handle repository not found properly

--- src/main/java/github/repositories/details/controller/GitHubRepositoryDetailsController.kt
+++ src/main/java/github/repositories/details/controller/GitHubRepositoryDetailsController.kt
@@ -4,7 +4,9 @@ import github.repositories.details.GitHubRepositoryDetails
 import github.repositories.details.GitHubRepositoryDetailsService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.HttpClientErrorException
 import java.util.*
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class GitHubRepositoryDetailsController @Autowired constructor(val service: GitHubRepositoryDetailsService) {
@@ -15,5 +17,10 @@ class GitHubRepositoryDetailsController @Autowired constructor(val service: GitH
             @PathVariable("repositoryName") repositoryName: String,
             locale: Locale): GitHubRepositoryDetails {
         return service.getRepositoryDetails(owner, repositoryName, locale)
+    }
+
+    @ExceptionHandler
+    fun handleIllegalArgumentException(e: HttpClientErrorException, response: HttpServletResponse) {
+        response.sendError(e.statusCode.value());
     }
 }
--- src/test/java/github/repositories/details/controller/GitHubRepositoryDetailsControllerTest.kt
+++ src/test/java/github/repositories/details/controller/GitHubRepositoryDetailsControllerTest.kt
@@ -14,13 +14,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.context.WebApplicationContext
 
 @RunWith(SpringJUnit4ClassRunner::class)
@@ -50,6 +54,13 @@ class GitHubRepositoryDetailsControllerTest {
     fun shouldReceivePathVariable() {
         mockMvc.perform(get("/repositories/owner/repositoryName"))
         verify(service).getRepositoryDetails(eq("owner"), eq("repositoryName"), any())
+    }
+
+    @Test
+    fun shouldShow404Error() {
+        whenever(service.getRepositoryDetails(any(), any(), any())).thenThrow(HttpClientErrorException(HttpStatus.NOT_FOUND))
+        mockMvc.perform(get("/repositories/owner/repositoryName").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound)
     }
 
     @After
