e3aaf55517e61e2482e799030a42628952a23912
Karol Kowalski <ordon1313@gmail.com>
2016-07-05 22:20:06 +0200

    Use FastDateFormat instead SimpleDateFormat

--- src/main/java/github/repositories/details/converter/GitHubApiDateFormatConverter.kt
+++ src/main/java/github/repositories/details/converter/GitHubApiDateFormatConverter.kt
@@ -1,14 +1,14 @@
 package github.repositories.details.converter
 
 import github.repositories.details.DateFormatConverter
+import org.apache.commons.lang3.time.FastDateFormat
 import java.text.DateFormat.FULL
 import java.text.DateFormat.getDateTimeInstance
-import java.text.SimpleDateFormat
 import java.util.*
 
 class GitHubApiDateFormatConverter : DateFormatConverter {
 
-    val gitHubDateFormater = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    val gitHubDateFormater = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss'Z'")
 
     override fun changeDateFormat(date: String, locale: Locale): String {
         return getDateTimeInstance(FULL, FULL, locale).format(gitHubDateFormater.parse(date))
